b527fab4582ad95774bff46422d3ce5d106798e2
José Victor <josevictor.s.scursulim@gmail.com>
2026-02-04 02:34:55 -0300

    agent update

--- src/home.py
+++ src/home.py
@@ -7,12 +7,18 @@ from langchain.messages import HumanMessage
 from agent import agent
 from utils import stream_data
 
-if not "config" in st.session_state:
+if "config" not in st.session_state:
     st.session_state.config = {
         "configurable": {
             "thread_id": str(uuid.uuid4())
         }
     }
+
+if "charts" not in st.session_state:
+    st.session_state.charts = {}
+
+if "dfs" not in st.session_state:
+    st.session_state.dfs = {}
 
 st.set_page_config(
     page_icon=":robot:",
@@ -24,26 +30,72 @@ st.title(":dollar: Yahoo Finance AI Agent :robot:")
 
 st.divider()
 
+st.warning(
+    body="""Important Notice:
+    This system is an experimental AI agent developed solely for academic and learning purposes. 
+    It is not a registered investment advisor and does not offer personalized or professional financial advice.
+    Any financial analysis or commentary produced by this agent should be treated as hypothetical and illustrative only. 
+    Users assume full responsibility for any actions taken based on its outputs.
+    """,
+    icon="⚠️"
+)
+
+st.divider()
+
 tab1, tab2 = st.tabs(["Chat", "Agent graph"])
 
 with tab1:
-    chat_container = st.container(border=True)
-    message = st.chat_input()
-    state = agent.get_state(config=st.session_state.config)
+    col1, col2 = st.columns([0.6, 0.4])
+    with col1:
+        chat_container = st.container(border=True)
+        message = st.chat_input()
+        state = agent.get_state(config=st.session_state.config)
 
-    if "messages" in state.values.keys():
-        for m in state.values["messages"]:
-            with chat_container.chat_message(name="human" if isinstance(m, HumanMessage) else "ai"):
-                st.write(m.content)
+        if "messages" in state.values.keys():
+            for m in state.values["messages"]:
+                with chat_container.chat_message(name="human" if isinstance(m, HumanMessage) else "ai", width="content"):
+                    st.write(m.content)
 
-    if message:
-        with chat_container.chat_message(name="human"):
-            st.write(message)
+        if message:
+            with chat_container.chat_message(name="human", width="content"):
+                st.write(message)
 
-        state = agent.invoke({"messages": [HumanMessage(content=message)]}, config=st.session_state.config)
+            with chat_container.chat_message(name="ai"):
+                with st.spinner(text="Processing your message..."):
+                    state = agent.invoke(
+                        {
+                            "messages": [HumanMessage(content=message)]
+                        },
+                        config=st.session_state.config
+                    )
 
-        with chat_container.chat_message(name="ai"):
-            st.write_stream(stream_data(text=state["messages"][-1].content))
+                if not isinstance(state["messages"][-1], HumanMessage):
+                    st.write_stream(stream_data(text=state["messages"][-1].content))
+    with col2:
+
+        if not st.session_state.charts == {}:
+            st.header("Charts history")
+            options = st.session_state.charts.keys()
+            chart_selected = st.selectbox(
+                label="Chart index", 
+                options=options, 
+                index=len(options)-1
+            )
+            st.plotly_chart(st.session_state.charts[chart_selected])
+
+        if not st.session_state.dfs == {}:
+            st.write("Data history")
+            options = st.session_state.dfs.keys()
+            df_selected = st.selectbox(
+                label="Data index", 
+                options=options, 
+                index=len(options)-1
+            )
+            st.dataframe(st.session_state.dfs[df_selected])
+
+    if st.toggle(label="Show debug"):
+        with st.expander(label="Debug", icon="🐞"):
+            st.write(agent.get_state(config=st.session_state.config))
 
 with tab2:
     st.image(agent.get_graph().draw_mermaid_png())
